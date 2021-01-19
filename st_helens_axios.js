13c47f123b32ff4b7eb91206f639c0c4ac2efc09
Darren Tynan <darren.tynan@gmail.com>
2021-01-19 15:01:12 +0000

    Code refactor. Added parse json to csv and file write.

--- st_helens_axios.js
+++ st_helens_axios.js
@@ -1,3 +1,6 @@
+const fs = require('fs').promises;
+const path = require('path');
+const { parse } = require('json2csv');
 const axios = require('axios');
 const cheerio = require('cheerio');
 const proxyGenerator = require("./modules/proxyGenerator");
@@ -5,6 +8,7 @@ const mongoose = require('mongoose');
 const StHelens = require('./model/StHelens');
 
 const targetURL = "https://www.sthelenschryslerdodgejeepram.com/cars-for-sale-warren-or.html";
+
 
 /**
  * Connect to mongoDB
@@ -96,6 +100,93 @@ function scrapeVehicleDetails($, collection)
 
 
 /**
+ * Parse the json from database and convert to csv before writing file.
+ * 
+ * @param  {mongoDB} database
+ */
+async function writeOutCSV(database)
+{
+    // For unique file name.
+    const filePath = path.join(__dirname, "/", "exports", "StHelens." + getCurrentTimeStamp() + ".csv");
+    // CSV field headers.
+    const fields = ['vin', 'make', 'model'];
+    // Options to be passed.
+    const opts = { fields };
+
+    database.once('open', () =>
+    {
+        // Fetch data from collection.
+        database.db.collection('vehicle', (error, collection) =>
+        {
+            collection.find({}).toArray( (error, data) =>
+            {
+                // console.log(data);
+
+                // Attempt to parse json and write file.
+                try
+                {
+                    const csv = parse(data, opts);
+                    // console.log(csv);
+
+                    fs.writeFile(filePath, csv, (error) =>
+                    {
+                        if (error) return console.log('error: ' + error);
+                    });
+
+                    console.log('File saved...');
+                }
+                catch (error)
+                {
+                    console.error(error);
+                }
+            });
+        });
+    });
+
+}
+
+
+/**
+ * Read the file.
+ * 
+ * @param  {string} filePath
+ */
+async function readFile(filePath)
+{
+    try
+    {
+        const data = await fs.readFile(filePath);
+        console.log(data.toString());
+    }
+    catch (error)
+    {
+        console.error(`Got an error trying to read the file: ${error.message}`);
+    }
+}
+
+  
+/**
+ * Get the current date timestamp.
+ * 
+ * @returns {string} YYYY-MM-DD.HH:MM:SS
+ */
+function getCurrentTimeStamp()
+{
+    let dateObj = new Date();
+
+    // Adjust 0 before single digit date.
+    let date = ('0' + dateObj.getDate()).slice(-2);
+    let month = ('0' + (dateObj.getMonth() + 1)).slice(-2);
+    let year = dateObj.getFullYear();
+    let hours = dateObj.getHours();
+    let minutes = ('0' + dateObj.getMinutes()).slice(-2);
+    let seconds = dateObj.getSeconds();
+
+    return date + '-' + month + '-' + year + '.' + hours + ':' + minutes + ':' + seconds;
+}
+
+
+/**
  * Initialisation
  */
 async function main()
@@ -103,8 +194,14 @@ async function main()
     // Call to connect to database.
     let database = connectToMongoDB();
 
-    // Scrape the parent page.
-    // await scrapeVehicleListings(database);
+    // Test function working!
+    // readFile('exports/test.txt');
+
+    // Scrape the parent page and store in database.
+    await scrapeVehicleListings(database);
+
+    // Fetch data from database and export to csv file.
+    writeOutCSV(database);
 
     // This will also exit after 1 seconds, and print its (killed) PID
     setTimeout(( () => { return process.kill(process.pid); }), 1000);
