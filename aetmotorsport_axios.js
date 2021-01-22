2290e1004555ff774d1e1f5283d6a793d34b8b1f
Darren Tynan <darren.tynan@gmail.com>
2021-01-22 11:06:14 +0000

    Second stage. Iterate over parent url list.

--- aetmotorsport_axios.js
+++ aetmotorsport_axios.js
@@ -6,8 +6,8 @@ const cheerio = require('cheerio');
 const proxyGenerator = require('./modules/proxyGenerator');
 const mongoose = require('mongoose');
 const AetMotorsport = require('./model/AetMotorsport');
-
-const targetURL = "https://www.aetmotorsport.com";
+const myData = [];
+const baseURL = "https://www.aetmotorsport.com";
 
 
 /**
@@ -25,14 +25,17 @@ function connectToMongoDB()
 
 
 /**
- * Scrape vehicle listing from parent page.
+ * Scrape links from parent page.
  * 
  * @param  {mongoDB} database
+ * @param  {String} collectionName
+ * @param  {String} url
+ * @param  {String} elementTarget
  */
-async function scrapeParentLinks(database)
+async function scrapePageLinks(database, collectionName, url, elementTarget)
 {
     console.log('Scraping parent links...');
-    await axios.get(targetURL, { proxy: proxyGenerator() })
+    await axios.get(url, { proxy: proxyGenerator() })
     .then( (response) =>
     {
         // console.log(response);
@@ -41,18 +44,18 @@ async function scrapeParentLinks(database)
             const $ = cheerio.load(response.data);
 
             // Iterate over menu and log all links.
-            $('#AccessibleNav > li > a').each((index, element) =>
+            $(elementTarget).each((index, element) =>
             {
-                console.log(targetURL +  $(element).attr('href'));
+                // console.log(targetURL +  $(element).attr('href'));
 
                 let newCollection = new AetMotorsport();
 
                 let endPoint = $(element).attr('href');
-                let dbElement = targetURL +  endPoint;
-                
+                let dbElement = baseURL +  endPoint;
+
                 newCollection.url = dbElement;
                 
-                database.collection('links').insertOne(newCollection);  
+                database.collection(collectionName).insertOne(newCollection);  
             });
 
         }
@@ -193,6 +196,37 @@ function getCurrentTimeStamp()
 
 
 /**
+ * Retrieve the collection from database.
+ * 
+ * @param  {} database
+ * @param  {} collectionName
+ */
+async function retrieveCollection(database, collectionName)
+{ 
+    
+    database.once('open', () =>
+    {
+        // Fetch data from collection.
+        database.db.collection(collectionName, async(error, collection) =>
+        {
+            await collection.find({}, { projection: { _id: 0, url: 1 } }).toArray( (error, data) =>
+            {
+                // console.log(data);
+                for (let i = 0; i < data.length; i++)
+                {
+                    myData.push(data[i].url);
+                }
+            });
+        });
+    });
+
+    console.log(myData);
+    return myData;
+
+}
+
+
+/**
  * Initialisation
  */
 async function main()
@@ -204,8 +238,12 @@ async function main()
     // readFile('exports/test.txt');
 
     // Scrape the parent page and store in database.
-    await scrapeParentLinks(database);
+//    await scrapePageLinks(database, 'all_menu_links', baseURL, '#AccessibleNav > li > a');
 
+    // Retrieve links from collection.
+    // const urls = await retrieveCollection(database, 'all_menu_links');
+    // console.log(urls);
+    await retrieveCollection(database, 'all_menu_links');
     // Fetch data from database and export to csv file.
     // writeOutCSV(database);
 
