5b4f45c0fe528952ec7c41b92716571c7d6c94d1
Darren Tynan <darren.tynan@gmail.com>
2021-01-23 19:14:40 +0000

    Moved models to their own folder.

---
+++ model/sthelens/StHelens.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const StHelensSchema = new mongoose.Schema({
+    vin: String,
+    make: String,
+    model: String,
+    year: String,
+    trim: String,
+    extcolor: String,
+    intcolor: String,
+    trans: String,
+    price: Number,
+    vehicleid: Number,
+    engine: String,
+    fueltype: String,
+    vehicletype: String,
+    bodystyle: String,
+    modelcode: String,
+    msrp: Number,
+    name: String,
+    cpo: String,
+    stocknum: String,
+    mpgcity: String,
+    mpghwy: String,
+});
+
+const StHelens = mongoose.model("StHelens", StHelensSchema);
+
+module.exports = StHelens;
