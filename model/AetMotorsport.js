bf0dac8e29a6a7bc4e71898936053895e1ff851a
Darren Tynan <darren.tynan@gmail.com>
2021-01-23 19:16:25 +0000

    Moved models, so no longer needed.

--- model/AetMotorsport.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const AetMotorsportSchema = new mongoose.Schema({
-    url: String,
-});
-
-const AetMotorsport = mongoose.model("AetMotorsport", AetMotorsportSchema);
-
-module.exports = AetMotorsport;
